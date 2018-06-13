263277d5101bd39107baf5b99fa7d967fc86adfe
gooLim <limdae07@gmail.com>
2018-06-14 02:35:59 +0900

    2-2

--- src/yoyo_2/Main.java
+++ src/yoyo_2/Main.java
@@ -1,38 +1,45 @@
 package yoyo_2;
 
+
 import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        LinkedList prime_number = new LinkedList();
-        int m = 0;
-        int n = -1;
-        int index = 0;
+        new Main();
+    }
+
+    public Main() {
+        Algorithm();
+    }
+    private void Algorithm() {
+        LinkedList prime_number = new LinkedList<Integer>();
         Scanner sc = new Scanner(System.in);
 
-        while ( m > n ) {
-            if ( m <= 0 || m > 10000 ) {
-                m = sc.nextInt();
-            }
-            if ( n <= 0 || n > 10000 ) {
-                n = sc.nextInt();
-            }
-        }
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int sum = 0;
 
-        index = m;
-        while ( index <= n ) {
+        while ( m <= n ) {
             int i = 2;
-            while ( i < index ) {
-                if (index % i == 0) {
+            while ( i < m ) {
+                if ( m % i == 0) {
                     break;
                 }
                 i++;
             }
-            if ( index == i ) {
-                prime_number.add(index);
+            if ( m == i ) {
+                prime_number.add(m);
+                sum += m;
             }
-            index++;
+            m++;
+        }
+        if( prime_number.size() == 0 ) {
+            System.out.println(-1);
+        }
+        else {
+            System.out.println(sum);
+            System.out.println(prime_number.pop());
         }
     }
 }
